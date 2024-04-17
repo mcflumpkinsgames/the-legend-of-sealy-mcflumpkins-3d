0cf98e67646713b872f65bb0b9ca721da606600c
mcflumpkinsgames <margaret.k.mcnulty+gamesgit@gmail.com>
2024-04-17 16:07:16 -0700

    movement shenanigans

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -1,21 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
 {
+    [SerializeField] InputAction movement;
+    [SerializeField] float xOffset = 0.1f;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    private void OnEnable() {
+        movement.Enable();
+    }
+
+    private void OnDisable() {
+        movement.Disable();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        float horizontalMovement = Input.GetAxis("Horizontal");
-        Debug.Log("horizontal movement: " + horizontalMovement);
-        float verticalMovement = Input.GetAxis("Vertical");
-        Debug.Log("Vertical movement: " + verticalMovement);
+        float xMovement = movement.ReadValue<Vector2>().x;
+        float yMovement = movement.ReadValue<Vector2>().y;
+
+        transform.localPosition = new Vector3 (transform.localPosition.x + xOffset, transform.localPosition.y, transform.localPosition.z);
     }
 }
