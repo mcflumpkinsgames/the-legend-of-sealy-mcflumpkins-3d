2fc90ca5ba777487eacf3148f7a70721343fa40f
mcflumpkinsgames <margaret.k.mcnulty+gamesgit@gmail.com>
2024-04-24 15:41:02 -0700

    rotation, but it doesn't realy work right now

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -2,11 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Windows.WebCam;
 
 public class PlayerControls : MonoBehaviour
 {
     [SerializeField] InputAction movement;
-    [SerializeField] float xOffset = 0.1f;
+    [SerializeField] float controlSpeed = 5f;
+    [SerializeField] float xRange = 3f;
+    [SerializeField] float yRange = 2f;
+
+    [SerializeField] float positionPitchFactor = 2f;
+    [SerializeField] float controlPitchFactor = 10f;
+
+    float xMovement;
+    float yMovement;
 
     // Start is called before the first frame update
     void Start()
@@ -22,12 +31,33 @@ public class PlayerControls : MonoBehaviour
         movement.Disable();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float xMovement = movement.ReadValue<Vector2>().x;
-        float yMovement = movement.ReadValue<Vector2>().y;
+        ProcessTranslation();
+        ProcessRotation();
+    }
 
-        transform.localPosition = new Vector3 (transform.localPosition.x + xOffset, transform.localPosition.y, transform.localPosition.z);
+    private void ProcessTranslation()
+    {
+        xMovement = movement.ReadValue<Vector2>().x;
+        yMovement = movement.ReadValue<Vector2>().y;
+
+        float xOffset = xMovement * controlSpeed * Time.deltaTime;
+        float yOffset = yMovement * controlSpeed * Time.deltaTime;
+
+        float rawXPos = transform.localPosition.x + xOffset;
+        float rawYPos = transform.localPosition.y + yOffset;
+
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+        float clampedYpos = Mathf.Clamp(rawYPos, -yRange, yRange);
+        transform.localPosition = new Vector3(clampedXPos, clampedYpos, transform.localPosition.z);
+    }
+
+    private void ProcessRotation()
+    {
+        float pitch = transform.localPosition.y * positionPitchFactor + yMovement * controlPitchFactor;
+        float yaw = 0f;
+        float roll = 0f;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 }
